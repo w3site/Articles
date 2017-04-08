7f2466113e4670c983aa464d2b368fa723d614b9
Alexander <tereta@mail.ua>
2017-04-08 19:25:14 +0300

    Licenses

--- Ui/Component/Listing/Column/Actions.php
+++ Ui/Component/Listing/Column/Actions.php
@@ -1,8 +1,18 @@
 <?php
 /**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * ╔╗╔╗╔╦══╦══╦══╦════╦═══╗╔══╦═══╦═══╗
+ * ║║║║║╠═╗║╔═╩╗╔╩═╗╔═╣╔══╝║╔╗║╔═╗║╔══╝
+ * ║║║║║╠═╝║╚═╗║║──║║─║╚══╗║║║║╚═╝║║╔═╗
+ * ║║║║║╠═╗╠═╗║║║──║║─║╔══╝║║║║╔╗╔╣║╚╗║
+ * ║╚╝╚╝╠═╝╠═╝╠╝╚╗─║║─║╚══╦╣╚╝║║║║║╚═╝║
+ * ╚═╝╚═╩══╩══╩══╝─╚╝─╚═══╩╩══╩╝╚╝╚═══╝
+ * 
+ * Examples and documentation at the: http://w3site.org
+ * 
+ * @copyright   Copyright (c) 2015-2016 Tereta Alexander. (http://www.w3site.org)
+ * @license     http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
+
 namespace WSite\Articles\Ui\Component\Listing\Column;
  
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
